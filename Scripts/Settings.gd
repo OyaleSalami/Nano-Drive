694fc94be500b224c808dc6d3a5f55f901e0a36e
Oyale <salamivictor15@gmail.com>
2024-11-06 22:04:36 +0100

    Finalizes the Settings Scene

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -3,18 +3,19 @@ extends Control
 func _ready():
 	LoadDefaultValues()
 
-func _on_Left_Button_down():
-	var _scene = self.get_tree().change_scene("res://Scene/Main Menu.tscn")
+func _process(_delta):
+	
+	pass
 
 func LoadDefaultValues():
-	var music = Globals.music
-	var sfx = Globals.sfx
-	
-	get_node("Settings Panel/VSplitContainer/SfxButton").pressed = sfx
-	get_node("Settings Panel/VSplitContainer/MusicButton").pressed = music
+	get_node("Settings Panel/VSplitContainer/SfxButton").pressed = Globals.sfx
+	get_node("Settings Panel/VSplitContainer/MusicButton").pressed = Globals.music
 
 func _on_SfxButton_toggled(button_pressed):
 	Globals.sfx = button_pressed
 
 func _on_MusicButton_toggled(button_pressed):
 	Globals.music = button_pressed
+
+func _on_Back_Button_down():
+	var _scene = self.get_tree().change_scene("res://Scene/Main Menu.tscn")
